532dc903746d27901d4a7abca2c66359046ab30e
aFleetingTime <liunian320@outlook.com>
2018-01-25 21:43:03 +0800

    test

--- Day14/Test/TestFile.cpp
+++ Day14/Test/TestFile.cpp
@@ -4,6 +4,8 @@
 #include <functional>
 #include <algorithm>
 #include <set>
+#include <list>
+#include <deque>
 //#include <std>
 
 using namespace std;
@@ -19,6 +21,15 @@ public:
 	int mAge;
 };
 
+template<class T>
+struct MyPrint
+{
+	void operator()(T &a)
+	{
+		cout << a << " ";
+	}
+};
+
 void test1()
 {
 	int a1[] = { 1,2,8,100};
@@ -30,9 +41,28 @@ void test1()
 	set_intersection(ivec1.begin(), ivec1.end(), ivec2.begin(), ivec2.end(), ivec3.begin());
 	transform(ivec3.begin(), ivec3.end(), ostream_iterator<int>(cout, " "), bind2nd(plus<int>(), 0));
 }
+
+void test2()
+{
+	list<int> l;
+	deque<int> s;
+	for (int i = 0; i < 10; ++i)
+	{
+		l.push_back(i);
+		s.push_back(i);
+	}
+	//random_shuffle(l.begin(), l.end());	random_shuffle不支持无法随机访问的容器
+	for_each(s.begin(), s.end(), MyPrint<int>());
+	cout << endl;
+	random_shuffle(s.begin(), s.end());
+	for_each(s.begin(), s.end(), MyPrint<int>());
+	cout << endl;
+}
+
 int main()
 {
-	test1();
+	//test1();
+	test2();
 
 	system("pause");
 	return 0;
