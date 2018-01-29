b629dff2829ca2b82ffd6536d81795f981e44434
aFleetingTime <liunian320@outlook.com>
2018-01-29 19:56:24 +0800

    thread

--- Day15/SingletonPattern/SingletonPattern.cpp
+++ Day15/SingletonPattern/SingletonPattern.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include <process.h>
+#include <mutex>
 
 using namespace std;
+
+static mutex cc;
 
 //懒汉式
 class Singleton1
@@ -21,27 +26,8 @@ private:
 	Singleton1() { cout << "Singleton1 Run..." << endl; }
 	~Singleton1() { cout << "~Singleton1 Run..." << endl; }
 	static Singleton1 *mInstance;
-	class MyGC
-	{
-	public:
-		MyGC()
-		{
-			cout << "GC1 Run..." << endl;
-		}
-		~MyGC()
-		{
-			if (mInstance != nullptr)
-			{
-				delete mInstance;
-				cout << "~GC1 Run..." << endl;
-			}
-			system("pause");
-		}
-	};
-	static MyGC gc;
 };
 Singleton1* Singleton1::mInstance = nullptr;
-Singleton1::MyGC Singleton1::gc;
 
 //饿汉式
 class Singleton2
@@ -59,30 +45,47 @@ private:
 	Singleton2() { cout << "Singleton2 Run..." << endl; }
 	~Singleton2() { cout << "Singleton2 Run..." << endl; }
 	static Singleton2 *mInstance;
-	class MyGC
-	{
-	public:
-		MyGC()
-		{
-			cout << "GC2 Run..." << endl;
-		}
-		~MyGC()
-		{
-			if (mInstance != nullptr)
-			{
-				delete mInstance;
-				cout << "~GC2 Run..." << endl;
-			}
-			system("pause");
-		}
-	};
-	static MyGC gc;
 };
 Singleton2* Singleton2::mInstance = new Singleton2;
-Singleton2::MyGC Singleton2::gc;
+
+//懒汉式
+class Singleton3
+{
+public:
+	static Singleton3 * getInstance()
+	{
+		if (mInstance == nullptr)
+		{
+			cc.lock();
+			if (mInstance == nullptr)
+			{
+				mInstance = new Singleton3;
+			}
+			cc.unlock();
+		}
+		return mInstance;
+	}
+	void show()
+	{
+		cc.lock();
+		cout << "懒汉式多线程测试..." << endl;
+		cc.unlock();
+	}
+private:
+	Singleton3() { cout << "Singleton3 Run..." << endl; }
+	~Singleton3() { cout << "~Singleton3 Run..." << endl; }
+	static Singleton3 *mInstance;
+};
+Singleton3* Singleton3::mInstance = nullptr;
+
+void test(void *)
+{
+	Singleton3::getInstance()->show();
+}
 
 int main()
 {
+#if 0
 	Singleton1 *s1 = Singleton1::getInstance();
 	s1->show();
 
@@ -96,7 +99,16 @@ int main()
 		cout << "单利" << endl;
 
 	cout << "-------------" << endl;
-
-	//system("pause");
+#endif
+	HANDLE threads[10];
+	for (int i = 0; i < 3; ++i)
+	{
+		threads[i] = reinterpret_cast<HANDLE>(_beginthread(test, 0, nullptr));
+	}
+	for (int i = 0; i < 3; ++i)
+	{
+		WaitForSingleObject(threads[i], INFINITE);
+	}
+	system("pause");
 	return 0;
 }
