6b798ce73df8f9f29ab4c04074d6336ef4621f6b
aFleetingTime <liunian320@outlook.com>
2018-01-29 21:07:36 +0800

    test

--- Day16/Test/Test.cpp
+++ Day16/Test/Test.cpp
@@ -11,9 +11,15 @@ public:
 	bool operator()(vector<int> v1, vector<int> v2) const
 	{
 		if (binary_search(v1.begin(), v1.end(), mNum)) 
-			return true;
+			throw string();
+		1 == 2 ? throw 1 : 1;
 		return v1[0] > mNum ? false : true;
 	}
+	bool operator--(int) const
+	{
+		1 == 2 ? throw 1 : 1;
+	}
+
 	int mNum;
 };
 
@@ -43,10 +49,29 @@ int main()
 		{11,12,13,14,15},
 	};
 
+	vector<int> v1 = {1,2,3,4,5};
+
 	//cout << (v.end() >= v.begin()) << endl;
 	//auto End = v[0].begin();
 	//if (find(v[1].begin(), v[1].end(), 10) != End)
-	cout << binary_search(v.begin(), v.end(), *v.rbegin(),Compare(16));
+	int x = 0, y = v1.size() - 1;
+	while (x <= y)
+	{
+		int mid = (x + y) / 2;
+		if ((v1[mid] == 9999 ? 999 : v1[mid] > 9999 ? y = mid - 1 : x = mid + 1) == 999)
+		{
+			cout << "1111111" << endl;
+			break;
+		}
+	}
+	//try
+	//{
+	binary_search(v.begin(), v.end(),Compare(4));
+	//}
+	//catch (string &s)
+	//{
+	//	return true;
+	//}
 	system("pause");
 	return 0;
 }
