e07a7ad460d8ac3ee8f6278690e297f98bbcff5b
aFleetingTime <liunian320@outlook.com>
2018-02-07 13:31:04 +0800

    valarray

--- Day20/Test/test.cpp
+++ Day20/Test/test.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <iterator>
 #include <random>
+#include <valarray>
+#include <array>
+#include <ObjectArray.h>
 
 using namespace std;
 
@@ -19,25 +22,30 @@ void initArray(int (*p)[y])
 void test1(int (*const &p)[10])
 {
 	//for(int (*a)[y] : p)
-	for (const auto &s : p)
-	{
-		for (auto a : s)
-			cout << a << ' ';
-		cout << endl;
-	}
+	//for (const auto &s : p)
+	//{
+	//	for (auto a : s)
+	//		cout << a << ' ';
+	//	cout << endl;
+	//}
 }
 
 int main()
 {
-	int array[x][y]{};
-	initArray(array);
-	test1(array);
-	for (const auto &p : array)
-	{
-		for (auto a : p)
-			cout << a << ' ';
-		cout << endl;
-	}
+	//int array[x][y]{};
+	array<int, x> A{1,2,3,4,5};
+	//initArray(array);
+	
+	valarray<int> a(A.data(), 5);
+	//a.apply();
+	//test1(array);
+	//for (const auto &p : array)
+	//{
+	//	for (auto a : p)
+	//		cout << a << ' ';
+	//	cout << endl;
+	//}
+	cout << (a += 10).min() << endl;
 
 	system("pause");
 	return 0;
