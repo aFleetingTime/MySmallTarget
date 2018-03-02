17d1e60e3a8cd5a55e7cad08d1ea23b7ea806bcd
aFleetingTime <liunian320@outlook.com>
2018-03-02 23:47:10 +0800

    up

--- Fstream/main.cpp
+++ Fstream/main.cpp
@@ -1,11 +1,12 @@
 #include "FileIO.h"
+#include <sstream>
 
 int main()
 {
-	std::vector<std::string> svec;
 	std::ifstream input("F:/a.txt");
 	if (!input)
 		std::cerr << "文件不存在" << std::endl;
+	std::vector<std::string> svec;
 	/*readline(input, svec);*/
 	readword(input, svec);
 	for (const std::string &str : svec)
