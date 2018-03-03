93ae47c9b47788a50e86dd906d8268bc8421b487
aFleetingTime <liunian320@outlook.com>
2018-03-03 16:39:32 +0800

    up

--- Fstream/main.cpp
+++ Fstream/main.cpp
@@ -1,16 +1,25 @@
 #include "FileIO.h"
 #include <sstream>
+#include <list>
+#include <forward_list>
+#include <deque>
 
 int main()
 {
-	std::ifstream input("F:/a.txt");
-	if (!input)
-		std::cerr << "文件不存在" << std::endl;
-	std::vector<std::string> svec;
-	/*readline(input, svec);*/
-	readword(input, svec);
-	for (const std::string &str : svec)
-		std::cout << str << std::endl;
+	std::list<std::string> info;
+	std::vector<int> a;
+	std::vector<int> b = a.begin();
+	for (std::string str; std::cin >> str; info.push_back(str));
+	for (const auto s : info) std::cout << s << std::endl;
+
+	//std::ifstream input("F:/a.txt");
+	//if (!input)
+	//	std::cerr << "文件不存在" << std::endl;
+	//std::vector<std::string> svec;
+	///*readline(input, svec);*/
+	//readword(input, svec);
+	//for (const std::string &str : svec)
+	//	std::cout << str << std::endl;
 
 	system("pause");
 	return 0;
